4c06962b9d81641c98b69cbd09a62a868cd78a96
Oystore <oystorecapstone@gmail.com>
2022-12-12 20:02:15 +0800

    deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import '../styles/styles.css'
 import Footer from '../components/Footer'
 import Navbar2 from '../components/Navbar2'
